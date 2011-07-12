d5e4398eaad2e06f6f8566c7b7a2d397fad39cf2
Carlmundo <djcjxp@hotmail.com>
2011-07-12 22:27:14 +0100

    Updated to v0.9.8.7

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.9.8.667")> 
+<Assembly: AssemblyVersion("0.9.8.7")> 
